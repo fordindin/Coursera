4073c9b7feb65370656893cd7d7ff334585e60a9
Denis Barov <dindin@dindin.ru>
2014-07-09 19:54:31 +0400

    progress

--- assignment1.py
+++ assignment1.py
@@ -15,11 +15,18 @@ cs = (
 
 target = "32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904"
 
+def msg2bs(m):
+		return [ m[i:i+2] for i in range(0,len(m),2) ]
+
+targetbs = msg2bs(target)
 def sxor(a,b):
-		out = ""
-		return [ chr(ord(c1)^ord(c2)) for c1,c2 in zip(a,b)]
+		a,b = msg2bs(a)[:len(targetbs)],msg2bs(b)[:len(targetbs)]
+		return [ chr(ord(x1.decode("hex"))^ord(x2.decode("hex"))).encode("hex") for x1,x2 in zip(a,b) ]
 
 def common_length(a,b):
 		return len(filter(lambda c: c[0]==c[1], zip(a,b)))
 
-print sxor(*cs[:2])
+
+print targetbs
+print sxor(cs[0],cs[1])
+
